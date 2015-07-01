01d67608a164ff47e496ea7331457459d4ce3625
David <davpaez@gmail.com>
2015-06-30 19:33:59 -0500

    New class for Realization data
    - Created DataRealization class, responsible for storing the output data of a realization
    - The Realization class constructs a DataRealization object in its report() method
    - The plot_history() function reads the DataRealization object and plots the figure

---
+++ source/+managers/DataRealization.m
@@ -0,0 +1,37 @@
+classdef DataRealization < handle
+    
+    properties
+        keywords
+        descriptions
+        values
+    end
+    
+    methods
+        
+        function self = DataRealization()
+            self.keywords = cell(0,1);
+            self.descriptions = cell(0,1);
+            self.values = cell(0,1);
+        end
+        
+        function addEntry(self, kw, desc, val)
+            self.keywords{end+1} = kw;
+            self.descriptions{end+1} = desc;
+            self.values{end+1} = val;
+        end
+        
+        function val = getValue(self, kw)
+            val = [];
+            for i=1:numel(self.keywords)
+                if strcmp(self.keywords{i}, kw)
+                    val = self.values{i};
+                    break
+                end
+            end
+        end
+        
+        
+    end
+    
+end
+
