438fbdf8a3553a83e29853a044384dea66bbb4c6
David <davpaez@gmail.com>
2015-09-02 01:20:36 -0500

    Changes to vol maint rules

--- source/+behavior/+agent/+VoluntaryMaint/Rule_4.m
+++ source/+behavior/+agent/+VoluntaryMaint/Rule_4.m
@@ -144,7 +144,7 @@ classdef Rule_4 < managers.DecisionRule
                 timeNextVolMaint = theAgent.time + 0.1;
             end
             
-            timeTermination = theAgent.contract.contractDuration;
+            timeTermination = theAgent.contract.duration;
             if timeNextVolMaint > timeTermination
                 if thisRule.finalMaint == false
                     timeNextVolMaint = max(timeTermination - 0.1, ...
