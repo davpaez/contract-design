41ef7c79bb9091a822de5ddcf4455e786ebb0ebb
David <davpaez@gmail.com>
2015-09-26 19:46:15 -0500

    Formalization of paper results
    - main.m script produces all results shown in paper, including plots

--- source/main.m
+++ source/main.m
@@ -16,14 +16,41 @@ addpath('experiments');
 %%
 
 % Experiment 1
-progSettings = ex6.settings();
+progSettings = ex1.settings();
 experim = Experiment(progSettings);
 
 experim.run();
 data1 = experim.report();
 
 %%
-%{
+
+% Experiment 2
+progSettings = ex2.settings();
+experim = Experiment(progSettings);
+
+experim.run();
+data2 = experim.report();
+
+%%
+
+% Experiment 3
+progSettings = ex3.settings();
+experim = Experiment(progSettings);
+
+experim.run();
+data3 = experim.report();
+
+%%
+
+% Experiment 4
+progSettings = ex4.settings();
+experim = Experiment(progSettings);
+
+experim.run();
+data4 = experim.report();
+
+%%
+
 % Experiment 5
 progSettings = ex5.settings();
 experim = Experiment(progSettings);
@@ -33,7 +60,6 @@ data5 = experim.report();
 
 %%
 
-
 % Experiment 6
 progSettings = ex6.settings();
 experim = Experiment(progSettings);
@@ -41,31 +67,122 @@ experim = Experiment(progSettings);
 experim.run();
 data6 = experim.report();
 
-%% 
+%%
+
+% Saving data
+
+save data1.mat data1
+save data2.mat data2
+save data3.mat data3
+save data4.mat data4
+save data5.mat data5
+save data6.mat data6
+
+%%
+
+% Loading data
+
+clear all
+clc
+
+load data1.mat
+load data2.mat
+load data3.mat
+load data4.mat
+load data5.mat
+load data6.mat
+
+%%
+
+% Plotting Experiment 1
+
+h1 = figure('Units', 'pixels', ...
+    'Position', [300 300 414 257],...
+    'Resize', 'on', ...
+    'Visible','on', ...
+    'Name', 'Experiment 1', ...
+    'Tag', 'exp1');
+p1 = uipanel(h1);
+gui.plot_history(p1, data1)
 
 
-xdata_exp5 = data5.getEntry('ua');
-ydata_exp5 = data5.getEntry('up');
+%%
 
-xdata_exp6 = data6.getEntry('ua');
-ydata_exp6 = data6.getEntry('up');
+% Plotting Experiment 2
 
-% h = figure('Units', 'pixels', ...
-%     'Position', [300 300 500 200],...
-%     'Resize', 'on', ...
-%     'Visible','on', ...
-%     'Tag', 'main');
-% 
-% gui.plot_dispersion(h, data5, {'ua', 'up'});
-% hold(h, 'on')
-% gui.plot_dispersion(h, data6, {'ua', 'up'});
+h6_SING = figure('Units', 'pixels', ...
+    'Position', [300 300 443 178],...
+    'Resize', 'on', ...
+    'Visible','on', ...
+    'Name', 'Experiment 2', ...
+    'Tag', 'exp2');
+p2 = uipanel(h6_SING);
+gui.plot_history(p2, data2)
 
-scatter(xdata_exp5.value, ydata_exp5.value, 'o')
-hold on
-scatter(xdata_exp6.value, ydata_exp6.value,'+')
-xlabel('Agent''s utility')
-ylabel('Principal''s utility')
+%%
 
-h1 = figure;
-gui.plot_dispersion(h1, data6, {'ua', 'up'});
-%}
+% Plotting Experiment 3
+
+h3 = figure('Units', 'pixels', ...
+    'Position', [300 300 443 178],...
+    'Resize', 'on', ...
+    'Visible','on', ...
+    'Name', 'Experiment 3', ...
+    'Tag', 'exp3');
+p3 = uipanel(h3);
+gui.plot_history(p3, data3)
+
+%%
+
+% Plotting Experiment 4
+
+h4 = figure('Units', 'pixels', ...
+    'Position', [300 300 443 178],...
+    'Resize', 'on', ...
+    'Visible','on', ...
+    'Name', 'Experiment 4', ...
+    'Tag', 'exp4');
+p4 = uipanel(h4);
+gui.plot_history(p4, data4)
+
+%%
+
+% Plotting Experiment 5 - Single realization
+
+h6_SING = figure('Units', 'pixels', ...
+    'Position', [300 300 443 178],...
+    'Resize', 'on', ...
+    'Visible','on', ...
+    'Name', 'Experiment 5 - SING', ...
+    'Tag', 'exp5_SING');
+p6_SING = uipanel(h6_SING);
+gui.plot_history(p6_SING, data5.source{1})
+
+
+%%
+
+% Plotting Experiment 6 - Single realization
+
+h6_SING = figure('Units', 'pixels', ...
+    'Position', [300 300 443 178],...
+    'Resize', 'on', ...
+    'Visible','on', ...
+    'Name', 'Experiment 6 - SING', ...
+    'Tag', 'exp6_SING');
+p6_SING = uipanel(h6_SING);
+gui.plot_history(p6_SING, data6.source{1})
+
+%%
+
+% Plotting scatter ua vs. up from experiments 5 and 6
+
+
+h56 = figure('Units', 'pixels', ...
+    'Position', [300 300 450 400],...
+    'Resize', 'on', ...
+    'Visible','on', ...
+    'Name', 'Comparison Exp 5 and Exp 6', ...
+    'Tag', 'exp56');
+
+p56 = uipanel(h56);
+gui.plot_dispersion_comparison(p56, data5, data6, {'ua', 'up'});
