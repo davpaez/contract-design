e4e9566a3d229892cb63a99b568c74dc164735c2
David <davpaez@gmail.com>
2015-09-26 20:32:42 -0500

    Comparison of histograms is done in a single axis

--- source/+gui/plot_dispersion_comparison.m
+++ source/+gui/plot_dispersion_comparison.m
@@ -5,19 +5,13 @@ assert(length(kw) == 2, 'Two keywords must be passed within kw argument')
 %% Axes
 
 % Axis - Histogram X
-ax1 = subplot('Position', [0.1 0.85 0.55 0.10], 'Parent', panel);
-
-% Axis - Data 1
-ax2 = subplot('Position', [0.1 0.70 0.55 0.10], 'Parent', panel);
+ax1 = subplot('Position', [0.1 0.85 0.70 0.10], 'Parent', panel);
 
 % Axis - Scatter
-ax3 = subplot('Position', [0.1 0.1 0.55 0.55], 'Parent', panel);
+ax2 = subplot('Position', [0.1 0.1 0.70 0.70], 'Parent', panel);
 
-% Axis - Data1
-ax4 = subplot('Position', [0.70 0.1 0.1 0.55], 'Parent', panel);
-
-% Axis - Data2
-ax5 = subplot('Position', [0.85 0.1 0.1 0.55], 'Parent', panel);
+% Axis - Histogram Y
+ax3 = subplot('Position', [0.85 0.1 0.1 0.70], 'Parent', panel);
 
 %% Data
 
@@ -35,40 +29,67 @@ y2_vector = y2_data.value;
 
 
 % Scatter
-hold(ax3,'on')
-scatter(ax3, x1_vector, y1_vector, 'o') % Data 1
-scatter(ax3, x2_vector, y2_vector, '+') % Data 2
+hold(ax2,'on')
+scatter(ax2, x1_vector, y1_vector, 'o') % Data 1
+scatter(ax2, x2_vector, y2_vector, '+') % Data 2
 
-x_lim = ax3.XLim;
-y_lim = ax3.YLim;
+x_lim = ax2.XLim;
+y_lim = ax2.YLim;
 
-x_bin_width = diff(x_lim)/30;
-y_bin_width = diff(y_lim)/30;
+x_bin_width = diff(x_lim)/nbins;
+y_bin_width = diff(y_lim)/nbins;
 
 % Histograms X
 
 hold(ax1, 'on')
-h1 = histogram(ax1, x2_vector);
-h2 = histogram(ax1, x1_vector);
+hist1 = histogram(ax1, x1_vector);
+hist2 = histogram(ax1, x2_vector);
 
-h1.Normalization = 'probability';
-h1.BinWidth = x_bin_width;
-h2.Normalization = 'probability';
-h2.BinWidth = x_bin_width;
+hist1.Normalization = 'probability';
+hist1.BinWidth = x_bin_width;
+hist2.Normalization = 'probability';
+hist2.BinWidth = x_bin_width;
 
 ax1.XLim = x_lim;
-ax2.XLim = x_lim;
 
 % Histograms Y
 
-histogram(ax4, y1_vector, nbins);
-histogram(ax5, y2_vector, nbins);
+hold(ax3, 'on')
+hist3 = histogram(ax3, y1_vector);
+hist4 = histogram(ax3, y2_vector);
 
-view(ax4, 90, -90)
-view(ax5, 90, -90)
+view(ax3, 90, -90)
 
-ax4.XLim = y_lim;
-ax5.XLim = y_lim;
+hist3.Normalization = 'probability';
+hist3.BinWidth = y_bin_width;
+hist4.Normalization = 'probability';
+hist4.BinWidth = y_bin_width;
 
+ax3.XLim = y_lim;
+
+% Styling
+
+set(ax1, ...
+    'Color', panel.BackgroundColor, ...
+    'XTick',[], ...
+    'Box', 'off', ...
+    'XColor', [0.4314  0.4314  0.4314],...
+    'YColor', [0.4314  0.4314  0.4314])
+
+set(ax3, ...
+    'Color', panel.BackgroundColor, ...
+    'Box', 'off', ...
+    'XTick',[], ...
+    'XColor', [0.4314  0.4314  0.4314],...
+    'YColor', [0.4314  0.4314  0.4314])
+
+set(ax2, ...
+    'XColor', [0.4314  0.4314  0.4314],...
+    'YColor', [0.4314  0.4314  0.4314])
+
+% Labels
+
+xlabel(ax2, x1_data.description)
+ylabel(ax2, y1_data.description)
 end
 
